9ac3b0119df69bf16533231dea0c566c065dbc7f
Siraj Paravoor <143482878+sparavoor@users.noreply.github.com>
2026-02-17 19:02:41 +0530

    Fix Vercel Deployment: Enable Prisma Accelerate in lib/prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,16 @@
 import { PrismaClient } from '@prisma/client'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+const prismaClientSingleton = () => {
+    return new PrismaClient().$extends(withAccelerate())
+}
 
-// @ts-ignore
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+type PrismaClientWithAccelerate = ReturnType<typeof prismaClientSingleton>
+
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClientWithAccelerate | undefined
+}
+
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
